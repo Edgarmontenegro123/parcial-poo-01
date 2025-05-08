8b5a678c0c14992fc815a3513278137af1b2aabe
Edgar <edgarmontenegro321@gmail.com>
2025-05-08 18:03:52 -0300

    🎯 Added validations and error messages to the Cliente class

--- parcial01.js
+++ parcial01.js
@@ -42,6 +42,16 @@ class Camioneta extends Vehiculo {}
 
 class Cliente {
     constructor(nombre, apellido, documento) {
+        if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre)) {
+            throw new Error('El nombre solo debe contener letras.');
+        }
+        if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(apellido)) {
+            throw new Error('El apellido solo debe contener letras.');
+        }
+        if(!/^\d{7,8}$/.test(documento)) {
+            throw new Error('El documento debe contener entre 7 y 8 dígitos numéricos.');
+        }
+
         this.nombre = nombre;
         this.apellido = apellido;
         this.documento = documento;
